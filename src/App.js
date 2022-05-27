4badd6c60af5e03b21aa3a21aec7e33fbc03b0d8
Andy Swinburne <andy.swinburne@purplebricks.com>
2022-05-27 11:31:05 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="savings-calculator">
+      <h2>You could save</h2>
+      <p>£5,000</p>
+      <button>Book a valuation</button>
     </div>
   );
 }
